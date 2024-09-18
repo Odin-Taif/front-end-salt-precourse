93496277ed15e5a53b75b6fded9c25be48c6691c
Odin Thif <mjd.thif@icloud.com>
2024-09-18 12:47:37 +0200

    adding usePathname hook to apply different style when active

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const links = [
   {
@@ -25,6 +27,7 @@ const links = [
 ];
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <>
       <div className="flex p-2 bg-gray-200">
@@ -33,7 +36,12 @@ const Navbar = () => {
             <Link
               key={link.name}
               href={link.href}
-              className="flex h-[48px] grow items-center justify-center mx-2 gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+              className={clsx(
+                "flex h-[48px] mx-2 grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                {
+                  "bg-sky-100 text-blue-600": pathname === link.href,
+                }
+              )}
             >
               <p className="hidden md:block">{link.name}</p>
             </Link>
